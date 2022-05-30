6581d24d7684aa6861e3024b5ce12e8159d657d6
iam-seiryu <yang2takahashi@icloud.com>
2022-05-31 00:36:33 +0900

    fix discrete fourirer transform and its inverse operation using matrix multiplication not for loop

--- idft1.m
+++ idft1.m
@@ -1,13 +1,9 @@
-function [f] = idft1(fourier_value)
-%idft1 1차원 데이터에 이산 역 푸리에 변환을 수행하는 함수
-%   fourier_value : 샘플링된 푸리에 함수
-M = size(fourier_value, 2);
-f = zeros(size(fourier_value));
-% exp(j2\pi\mu x/M) = cos(2\pi\mu x/M) + jsin(2\pi\mu x/M)
-for i=1:size(f, 2)
-    for j=1:size(fourier_value, 2)
-        f(i) = f(i) + fourier_value(j) * (cos(2*pi*(i - 1)*(j - 1)/M) + 1i*sin(2*pi*(i - 1)*(j - 1)/M));
-    end
-end
-f = f / M;
+%% inverse discrete fourier transform 1D
+function [f] = idft1(freq_domain_value)
+M = length(freq_domain_value);
+x = 0:M-1;
+n = 0:M-1;
+power = 1i*2*pi/M*x'*n;
+expn = exp(power);
+f = real(freq_domain_value*expn/M);
 end
