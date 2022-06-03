3e4dffc024f6e7cf02b08b96505fb5c2b7f1dadd
iam-seiryu <yang2takahashi@icloud.com>
2022-06-03 22:11:59 +0900

    fix return value: row layout to column layout

--- dft1.m
+++ dft1.m
@@ -1,9 +1,9 @@
-%% discrete fourier transform
+%% discrete fourier transform 1D
 function [F] = dft1(time_domain_value)
-M = length(time_domain_value);
-x = 0:M-1;
-n = 0:M-1;
-power = -1i*2*pi/M*n'*x;
-expn = exp(power);
-F = time_domain_value*expn;
+    M = length(time_domain_value);
+    x = -M/2:M/2-1; 
+    n = -M/2:M/2-1;
+    power = -1i*2*pi/M*n'*x;
+    expn = exp(power);
+    F = (time_domain_value*expn)';
 end
--- idft1.m
+++ idft1.m
@@ -1,9 +1,9 @@
 %% inverse discrete fourier transform 1D
 function [f] = idft1(freq_domain_value)
-M = length(freq_domain_value);
-x = 0:M-1;
-n = 0:M-1;
-power = 1i*2*pi/M*x'*n;
-expn = exp(power);
-f = real(freq_domain_value*expn/M);
+    M = length(freq_domain_value);
+    x = -M/2:M/2-1;
+    n = -M/2:M/2-1;
+    power = 1i*2*pi/M*x'*n;
+    expn = exp(power);
+    f = real(freq_domain_value*expn/M)';
 end
