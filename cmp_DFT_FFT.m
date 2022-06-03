6de522d040af1ab9b6c22439bc9b653d54fe273e
iam-seiryu <yang2takahashi@icloud.com>
2022-06-03 22:17:12 +0900

    add frequency-domain plot to compare DFT and FFT

--- cmp_DFT_FFT.m
+++ cmp_DFT_FFT.m
@@ -8,13 +8,32 @@ load(mat_name+".mat", 'x', 'y');
 %% DFT
 tic;
 % x component
-x_dft = dft1(x);
+x_dft = dft1(x)';
 x_idft = idft1(x_dft);
 
 % y component
-y_dft = dft1(y);
+y_dft = dft1(y)';
 y_idft = idft1(y_dft);
 
+figure(1);  movegui("northwest");
+subplot(221);
+stem(real(x_dft))
+title("DFT of real part of x");
+xlabel('w');
+subplot(222);
+stem(imag(x_dft))
+title("DFT of imaginary part of x");
+xlabel('w');
+subplot(223);
+stem(real(y_dft))
+title("DFT of real part of y");
+xlabel('w');
+subplot(224);
+stem(imag(y_dft))
+title("DFT of imaginary part of y");
+xlabel('w');
+
+figure(2);  movegui("northeast");
 plot(x_idft,y_idft)
 
 elapsed_time = toc;
@@ -29,7 +48,26 @@ x_ifft = ifft(x_fft);
 y_fft = fft(y');
 y_ifft = ifft(y_fft);
 
-plot(x_ifft, y_ifft)
 
+figure(1);  movegui("northwest");
+subplot(221);
+stem(real(x_fft))
+title("FFT of real part of x");
+xlabel('w');
+subplot(222);
+stem(imag(x_fft))
+title("FFT of imaginary part of x");
+xlabel('w');
+subplot(223);
+stem(real(y_fft))
+title("FFT of real part of y");
+xlabel('w');
+subplot(224);
+stem(imag(y_fft))
+title("FFT of imaginary part of y");
+xlabel('w');
+
+figure(2);  movegui("northeast");
+plot(x_ifft,y_ifft)
 elapsed_time = toc;
 disp("elapsed time of : " + elapsed_time);
